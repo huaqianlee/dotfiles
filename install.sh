579f299cc9e55192ee75d2cb2b4e3f2de985da34
huaqianlee <huaqian@gmail.com>
2021-05-17 00:22:59 +0800

    First version

--- install.sh
+++ install.sh
@@ -1,55 +1,63 @@
 #!/bin/sh
 # Powered by AndyLee
 
-STOW_DIR=/home/lee
+LEE_HOME=/home/lee
+STOW_DIR=$LEE_HOME
+ZSH_CUSTOM=$LEE_HOME/.oh-my-zsh
 
 #####################################################################
-# Install the software packages and dependencies.
-#####################################################################
-# clipboard
-# apt -y install xsel
-
-# Terminator
-# apt -y install terminator
-
-# Zsh
-# apt -y install zsh
-
-
-# Oh My Zsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# Install Powerline fonts
-# apt -y install fonts-powerline
-
-
-# Install python package for terminator themes
-# pip install requests
-
-#####################################################################
-# Terminal setting.
-#####################################################################
-# Required root password
-chsh -s $(which zsh)
-
-#####################################################################
-# Copy the configuration.
+# Install packdges and setting
 #####################################################################
 #cp -rfT .* ~/
 # bash
-stow bash -t $STOW_DIR
+#stow bash -t $STOW_DIR
+# Uninstall first
+stow -D zsh -t $STOW_DIR
+stow -D vim -t $STOW_DIR
+stow -D git -t $STOW_DIR
+stow -D tmux -t $STOW_DIR
+stow -D oh-my-zsh -t $STOW_DIR
 
 # zsh
+echo "Installing zsh ..."
+# apt -y install zsh
 stow zsh -t $STOW_DIR
-# stow -D zsh -t $STOW_DIR
+eval `chsh -s $(which zsh)`
 
 # vim
+echo "Installing vim ..."
 stow vim -t $STOW_DIR
 
 # git
+echo "Installing git ..."
 stow git -t $STOW_DIR
 
 # tmux
+echo "Installing tmux ..."
 stow tmux -t $STOW_DIR
 
-# terminator
+# Terminator
+# apt -y install terminator
 # stow terminator -t $STOW_DIR
+# git clone git://github.com/sigurdga/gnome-terminal-colors-solarized.git ~/.solarized
+# cd ~/.solarized ; ./install.sh
+
+# Oh My Zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "Installing oh My Zsh ..."
+#eval "$LEE_HOME/.oh-my-zsh/tools/install.sh"
+rm -rf $LEE_HOME/.oh-my-zsh
+eval "zsh/.oh-my-zsh/tools/install.sh"
+#sh -c "$LEE_HOME/.oh-my-zsh/tools/install.sh"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+# clipboard
+# apt -y install xsel
+
+
+# Install Powerline fonts
+# apt -y install fonts-powerline
+
+# Install python package for terminator themes
+# pip install requests
